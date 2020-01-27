7028d94c5590d29d66c29e77915c90cdcf0558ec
Melissa <58809268+zudo-13ram@users.noreply.github.com>
2020-01-27 10:32:17 +0100

    Update Increment-decrement.js

--- Increment-decrement.js
+++ Increment-decrement.js
@@ -1,3 +1,45 @@
+//-------Small example of incrementation
+//INCREMENT
+/*
+let p = 0;
+p++;
+let c = p;
+console.log(c);
+*/
+//result
+//1
+/*
+a_THE VALUE OF p is cero' 
+b_i incremented 1 with p++
+c_now p is one, but i still cant see it in the console
+d_now the Value of c is one because of p'
+e_when i console log C' it will show me the 
+result of c has a value 1.
+//DECREMENT
+*/
+
+let h = 0;
+++h;
+let e = h;
+console.log(e);
+
+/*
+Suppose p=5;
+
+When we assign x=++p the increment
+ operation is carried out first and 
+ then the value of p is assigned to x. 
+ So both p and x are set to 6
+
+When we assign x=p++ the increment 
+operation is carried out after the
+ value of p is assigned to x. So x is 
+ set to 5 while p is incremented to 6.
+
+*/
+
+//----------------------------
+
 /*
 let num1 = 1;
 console.log(num1);

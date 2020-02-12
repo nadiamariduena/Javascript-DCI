f46440611996189864808740873537a39c4e4b80
Nadia Mariduena <58809268+nadiamariduena@users.noreply.github.com>
2020-02-12 15:57:58 +0100

    Update This_keyword.js

--- This_keyword.js
+++ This_keyword.js
@@ -8,6 +8,22 @@
 // function -> global (window, global)
 //--------------(1)--------------------
 
+const video = {  //video is an OBJECT
+  linear: "a",   //linear is a PROPERTY
+  play() {       //play is a METHOD   
+    console.log(this);
+  }
+};
+
+video.stop = function() {
+  console.log(this);
+};
+
+video.stop();
+
+//-----------------
+
+
 const video = {
   linear: "a",
   play() {
@@ -20,6 +36,47 @@ video.stop = function() {
 };
 
 video.stop();
+
+//If you check the console you will see that both methods (play and stop)
+//will appear and thats due au fait que play and stop are METHODS in the video OBJECT.
+
+
+/*--------playing-with it-but wrong------my test----------------*/
+/*--------------------------------------------------------------
+
+const videoClash = {
+  linear: "a",
+  play() {
+    console.log(this);
+  },
+  linear2: "b",
+  bur() {
+    console.log(this);
+  }
+};
+
+videoClash.stop = function() {
+  console.log(this);
+};
+
+videoClash.stop();
+videoClash.play();
+{
+  linear: 'a',
+  play: [Function: play],
+  linear2: 'b',
+  bur: [Function: bur],
+  stop: [Function (anonymous)]
+}
+{
+  linear: 'a',
+  play: [Function: play],
+  linear2: 'b',
+  bur: [Function: bur],
+  stop: [Function (anonymous)]
+}
+*/
+/*-----------------------------------------*/
 
 //-------------------------------------
 //function -> GLOBAL (window, global)
